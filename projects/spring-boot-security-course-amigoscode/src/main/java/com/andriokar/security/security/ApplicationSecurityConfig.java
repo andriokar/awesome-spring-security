f53ec6f5b07ad587d3de36c229aad9156ad9d259
Andriy <karpus.andrii@proton.me>
2025-03-27 16:50:35 +0100

    Refactor config

--- projects/spring-boot-security-course-amigoscode/src/main/java/com/andriokar/security/security/ApplicationSecurityConfig.java
+++ projects/spring-boot-security-course-amigoscode/src/main/java/com/andriokar/security/security/ApplicationSecurityConfig.java
@@ -2,10 +2,10 @@ package com.andriokar.security.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -14,15 +14,15 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class ApplicationSecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers(new AntPathRequestMatcher("/api/v1/students/*"))
-                        .authenticated()
-                        .anyRequest()
-                        .permitAll()
+    protected SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests((auth) ->
+                        auth
+                                .requestMatchers(HttpMethod.GET, "/api/v1/students/*").authenticated()
+                                .anyRequest().permitAll()
                 )
-                .httpBasic(withDefaults())
-                .build();
+                .httpBasic(withDefaults());
+
+        return httpSecurity.build();
     }
 }
