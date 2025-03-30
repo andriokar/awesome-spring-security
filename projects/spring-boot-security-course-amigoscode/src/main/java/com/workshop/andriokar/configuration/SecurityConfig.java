f8cac4633be39120c6eb17cc89ef30c52d039ee3
Andriy <karpus.andrii@proton.me>
2025-03-30 10:16:23 +0200

    Implement JWT authentication

--- projects/spring-boot-security-course-amigoscode/src/main/java/com/workshop/andriokar/configuration/SecurityConfig.java
+++ projects/spring-boot-security-course-amigoscode/src/main/java/com/workshop/andriokar/configuration/SecurityConfig.java
@@ -1,16 +1,19 @@
 package com.workshop.andriokar.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -18,7 +21,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests((auth) -> auth
                         .anyRequest().authenticated()
                 )
-                .httpBasic(withDefaults());
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
         return httpSecurity.build();
     }
 }
