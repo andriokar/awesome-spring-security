200cc21487379af57dee721d591cc65f831d9b8d
Andriy <karpus.andrii@proton.me>
2025-03-30 10:26:43 +0200

    Implement UserDetailsService

--- projects/spring-boot-security-course-amigoscode/src/main/java/com/workshop/andriokar/configuration/SecurityConfig.java
+++ projects/spring-boot-security-course-amigoscode/src/main/java/com/workshop/andriokar/configuration/SecurityConfig.java
@@ -5,8 +5,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -14,6 +23,19 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
+
+    private final static List<UserDetails> APPLICATION_USERS = List.of(
+            new User(
+                    "admin@test.com",
+                    "password",
+                    Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))
+            ),
+            new User(
+                    "user@test.com",
+                    "password",
+                    Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))
+            )
+    );
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -24,5 +46,18 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username ->
+                APPLICATION_USERS.stream()
+                        .filter(userDetails -> userDetails.getUsername().equals(username))
+                        .findFirst()
+                        .orElseThrow(
+                                () -> new UsernameNotFoundException(
+                                        "User with username %s does not exist".formatted(username)
+                                )
+                        );
     }
 }
