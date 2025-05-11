9c359d14b81037e53c1d81c1b21eb56e2b0bd194
Andriy <karpus.andrii@proton.me>
2025-05-11 22:31:13 +0200

    Create new project

---
+++ projects/spring-security-course-telusko/src/test/java/com/andriokar/springsecex/SpringSecExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andriokar.springsecex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
