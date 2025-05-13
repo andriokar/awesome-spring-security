08bb815aed547c213b88d2c623c94237d8eba894
Andriy <karpus.andrii@proton.me>
2025-05-13 15:51:23 +0200

    Get and print session id

--- projects/spring-security-course-telusko/src/main/java/com/andriokar/springsecex/HelloController.java
+++ projects/spring-security-course-telusko/src/main/java/com/andriokar/springsecex/HelloController.java
@@ -1,5 +1,6 @@
 package com.andriokar.springsecex;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 @GetMapping("/")
-    public String greet() {
-        return "Welcome to Telusko";
+    public String greet(HttpServletRequest httpServletRequest) {
+        return "Welcome to Telusko, your sessionId is %s".formatted(httpServletRequest.getSession().getId());
     }
 
 }
