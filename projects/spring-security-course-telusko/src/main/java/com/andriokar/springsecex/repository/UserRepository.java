842d76f8f87447ab6d432c5d5736d036ff4de454
Andriy <karpus.andrii@proton.me>
2025-05-18 00:01:23 +0200

    Create user model and repository

---
+++ projects/spring-security-course-telusko/src/main/java/com/andriokar/springsecex/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.andriokar.springsecex.repository;
+
+import com.andriokar.springsecex.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
